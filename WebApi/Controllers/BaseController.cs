8f1e92fbf4ffbfde2e775b714193562e470cc854
Berat AK <brt_ak_61@hotmail.com>
2023-08-03 16:38:20 +0300

    Add MediatR

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+
+        //Yani daha önce mediator enjekte edilmişse onu döndür edilmemişse de set ediyor
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>();
+
+
+    }
+}
+
+// protected = sadece inherit edenler kullansın
+// aynı zamanda protected olduğu için büyük harf ile başlıyor
